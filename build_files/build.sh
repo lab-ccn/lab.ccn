abdd906527c0fa050575b83abee23d5a9e948d45
Azerxal <joshuabambridge93@gmail.com>
2026-04-15 15:09:49 +0100

    Install Putty and Remmina

--- build_files/build.sh
+++ build_files/build.sh
@@ -19,6 +19,11 @@ dnf5 install -y tmux
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
+dnf5 -y install https://download.virtualbox.org/virtualbox/7.2.6/VirtualBox-7.2-7.2.6_172322_fedora40-1.x86_64.rpm
+dnf5 -y install libreoffice
+dnf5 -y install putty
+dnf5 -y install remmina
+
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
