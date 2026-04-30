c1e43945d90b2d52735450c1477b79493c029b04
Juniper Gardiner <86971433+JunePrimavera@users.noreply.github.com>
2026-04-30 13:23:09 +0100

    Download and extract PacketTracer files to /tmp

--- build_files/build.sh
+++ build_files/build.sh
@@ -71,13 +71,13 @@ EOF
 # Packet Tracer Dependencies
 dnf5 -y install qt5-qtnetworkauth qt5-qtscript qt5-qtmultimedia qt5-qtwebsockets qt5-qtwebengine
 # Download and extract packet tracer .deb archive
-curl "https://www.netacad.com/authoring-resources/courses/ff9e491c-49be-4734-803e-a79e6e83dab1/c3636211-1ce6-4f92-8a22-ccddf902dd72/en-US/assets/PacketTracer822_amd64_signed_en-US_35234a27-3127-49bc-91ce-2926af76f07a.deb" -o pt.deb
-ar -x pt.deb
-tar -xf data.tar.xz
+curl "https://www.netacad.com/authoring-resources/courses/ff9e491c-49be-4734-803e-a79e6e83dab1/c3636211-1ce6-4f92-8a22-ccddf902dd72/en-US/assets/PacketTracer822_amd64_signed_en-US_35234a27-3127-49bc-91ce-2926af76f07a.deb" -o /tmp/pt.deb
+ar -x /tmp/pt.deb --output=/tmp
+tar -xf /tmp/data.tar.xz -C /tmp
 # Copy PacketTracer binary and libssl/libcrypto - Fedora no longer packages openssl 1.1, so we use what has been bundled with PacketTracer.
-cp opt/pt/bin/PacketTracer /usr/lib/
-cp opt/pt/bin/libssl.so.1.1 /usr/lib
-cp opt/pt/bin/libcrypto.so.1.1 /usr/lib
+cp /tmp/opt/pt/bin/PacketTracer /usr/lib/
+cp /tmp/opt/pt/bin/libssl.so.1.1 /usr/lib
+cp /tmp/opt/pt/bin/libcrypto.so.1.1 /usr/lib
 # Copy desktop entry for PacketTracer
 echo """
 [Desktop Entry]
@@ -89,7 +89,7 @@ StartupNotify=false
 Terminal=false
 """ >> /usr/share/applications/PacketTracer.desktop
 # Copy the packettracer start script into /usr/bin - You can't just run the binary directly on Fedora.
-cp opt/pt/packettracer /usr/bin/
+cp /tmp/opt/pt/packettracer /usr/bin/
 
 # Additional Software
 dnf5 -y install libreoffice putty remmina filezilla tmux btop fastfetch hyfetch picocom bat kitty chromium
