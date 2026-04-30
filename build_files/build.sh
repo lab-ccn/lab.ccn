4455242e007dd765865dfa9337070e9ba54ffd4d
Juniper Gardiner <86971433+JunePrimavera@users.noreply.github.com>
2026-04-30 12:03:49 +0100

    Fix formatting

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,26 +8,21 @@ KERNEL_VER=${KERNEL_PKG_VER#"kernel-"}
 set -ouex pipefail
 
 # VirtualBox
+# Automatically detect the latest fedora RPM release from VirtualBox
 VIRTUALBOX_VER="$(curl -L https://download.virtualbox.org/virtualbox/LATEST.TXT)"
-# URL to list of VirtualBox packages for latest version
 VIRTUALBOX_VER_URL="https://download.virtualbox.org/virtualbox/$VIRTUALBOX_VER/"
-# get all available VirtualBox Fedora rpm packages, sorted descending, and loop through them
 VIRTUALBOX_RPMS="$(curl -L "$VIRTUALBOX_VER_URL" | grep -E -o 'VirtualBox.+?fedora[0-9]+?-.+?\.x86_64\.rpm' | sed -E -e 's/">.*//' | sort -Vr)"
 for _VIRTUALBOX_RPM in $VIRTUALBOX_RPMS; do
-  # extract the Fedora version from the file name
   FEDORA_VERSION="$(echo $_VIRTUALBOX_RPM | grep -E -o 'fedora[0-9]+' | grep -E -o '[0-9]+')"
-  # if <= $RELEASE, break
   if [[ "$FEDORA_VERSION" -le "$RELEASE" ]]; then
     VIRTUALBOX_RPM="$_VIRTUALBOX_RPM"
     break
   fi
 done
-# URL to VirtualBox rpm
 VIRTUALBOX_RPM_URL="$VIRTUALBOX_VER_URL$VIRTUALBOX_RPM"
 echo "Using '$VIRTUALBOX_RPM_URL' for Fedora $RELEASE"
-# download VirtualBox rpm
+# Download and install VirtualBox rpm
 curl -L -o "/tmp/$VIRTUALBOX_RPM" "https://download.virtualbox.org/virtualbox/$VIRTUALBOX_VER/$VIRTUALBOX_RPM"
-# install VirtualBox
 dnf install -y "/tmp/$VIRTUALBOX_RPM"
 
 # The kernel during a build is not the same as the resulting fedora kernel! 
@@ -52,13 +47,10 @@ if [[ -e /var/log/vbox-setup.log ]]; then
 fi
 
 EXTPACK_NAME="Oracle_VirtualBox_Extension_Pack-$VIRTUALBOX_VER.vbox-extpack"
-# sha256 sums URL
 SUMS_URL="${VIRTUALBOX_VER_URL}SHA256SUMS"
-# sha256 for extpack file
 HASH="$(curl -L $SUMS_URL | grep $EXTPACK_NAME | cut -d' ' -f1)"
-# extension pack URL
 EXTPACK_URL="${VIRTUALBOX_VER_URL}${EXTPACK_NAME}"
-# download and install extension pack
+# Install the VirtualBox extension pack
 EXTPACK_PATH="/tmp/extpack.vbox-extpack"
 curl -L -o $EXTPACK_PATH "$EXTPACK_URL"
 /usr/lib/virtualbox/VBoxExtPackHelperApp install \
@@ -67,6 +59,7 @@ curl -L -o $EXTPACK_PATH "$EXTPACK_URL"
   --name "Oracle VirtualBox Extension Pack" \
   --tarball $EXTPACK_PATH \
   --sha-256 $HASH
+# Load Kernel Modules
 mkdir -p /usr/lib/modules-load.d
 cat > /usr/lib/modules-load.d/kinoite-virtualbox.conf << EOF
 # load virtualbox kernel drivers
